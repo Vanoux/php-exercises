92ff29c083847ffb1b00c0ee94636f94e1bb5329
Vanoux <vanessa.roux891@orange.fr>
2018-07-23 23:12:04 +0200

    exo 4 Array mois

--- 3_Array/Exercice4.php
+++ 3_Array/Exercice4.php
@@ -3,5 +3,6 @@
 modifier le mois de aout pour lui ajouter l'accent manquant.
 */
 $mois = array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
-
+$mois[7] = "août";
+echo $mois[7]. "\n";
 ?>
