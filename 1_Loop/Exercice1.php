c71d9e37d1a12e1c40579cca380892c4059e58c0
Vanoux <vanessa.roux891@orange.fr>
2018-07-23 13:04:45 +0200

    ajout dossier exo loop php

---
+++ 1_Loop/Exercice1.php
@@ -0,0 +1,13 @@
+<?php 
+/*  Créer une variable et l'initialiser à 0.
+Tant que cette variable n'atteint pas 10, il faut :
+
+    l'afficher
+    l'incrementer
+*/
+
+$number = 0;
+while ($number < 11) {
+    echo $number++ ."\n";
+}
+?>
---
+++ 1_Loop/Exercice2.php
@@ -0,0 +1,17 @@
+<?php 
+/* Créer deux variables. 
+Initialiser la première à 0.
+Et la deuxième avec un nombre compris en 1 et 100.
+Tant que la première variable n'est pas supérieur à 20 :
+
+    multiplier la première variable avec la deuxième
+    afficher le résultat
+    incrementer la première variable
+*/
+
+$num1 = 0;
+$num2 = 50;
+while ($num1 > 20) {
+    echo $num1 * $num2 ."\n";
+}
+?>
